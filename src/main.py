737bfe3d6f067c3c33ecb4c3145a2846e6f391bf
CarCedo <CarCedo.Pro@gmail.com>
2022-09-02 08:14:42 +0430

    Basic database loading (sqlite) implemented

--- src/main.py
+++ src/main.py
@@ -1,6 +1,17 @@
+from pathlib import Path
+
 from flask import Flask, render_template
+from models import db
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///main.db"
+
+db.app = app
+db.init_app(app)
+
+# Check if the db file has never been created, create one
+if not Path("main.db").is_file():
+	db.create_all()
 
 @app.route("/")
 def index_page():
