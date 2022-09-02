6e5d112ceeeaddb43abd08db2b022ff2ab84fa84
CarCedo <CarCedo.Pro@gmail.com>
2022-09-02 08:21:28 +0430

    Added todo model

--- src/models.py
+++ src/models.py
@@ -1,3 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
+
+class Todo(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	content = db.Column(db.String(200), nullable = False)
+	date_created = db.Column(db.DateTime, default = datetime.utcnow)
