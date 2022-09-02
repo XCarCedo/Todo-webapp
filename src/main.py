3e068e45bd126929c61452119a15ff2e156e3b72
CarCedo <CarCedo.Pro@gmail.com>
2022-09-02 09:00:21 +0430

    Added basic delete feature

--- src/main.py
+++ src/main.py
@@ -15,6 +15,9 @@ if not Path("main.db").is_file():
 
 @app.route("/", methods = ["GET", "POST"])
 def index_page():
+	"""Basically if the request is get list of todos shown otherwise the user
+	has used the form to add another todo and will be redirected to index page again
+	"""
 	if request.method == "POST":
 		content = request.form['content']
 		db.session.add(Todo(content = content))
@@ -22,6 +25,19 @@ def index_page():
 		return redirect("/")
 	else:
 		return render_template("index.html", todos = Todo.query.order_by(Todo.date_created).all())
+
+@app.route("/delete/<int:record_id>")
+def delete_page(record_id: int):
+    """Delete todo based on the id given from the link
+    
+    Args:
+        id (int): The id of todo wanted to be deleted
+    """
+    todo_record = Todo.query.get_or_404(record_id)
+    db.session.delete(todo_record)
+    db.session.commit()
+    return redirect("/")
+	
 
 if __name__ == "__main__":
 	app.run(debug=True)
